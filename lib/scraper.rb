74c886011946e41cbc7463a0b99f4fa8b0c874f5
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-12-15 16:50:07 -0500

    commit tree 781f17e2c7fd2cb281a444214f653a8951798864

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,19 @@ require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        profile_link = "#{student.attr('href')}"
+        location = student.css(".student-location").text
+        name = student.css(".student-name").text
+        students << {name: name, location: location, profile_url: profile_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
